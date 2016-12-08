b1894e3ab8903088d31ceb52faf0dec8f9aa6696
Andre <andre_vd_velde@hotmail.com>
2016-12-08 14:26:43 +0100

    Layout changes

--- MoodTracker/Resources/Resource.Designer.cs
+++ MoodTracker/Resources/Resource.Designer.cs
@@ -9,9 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using Android.App;
-
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("MoodTracker.Resource", IsApplication=true), Application(Icon = "@drawable/Icon", Theme = "@android:style/Theme.Material.Light")]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("MoodTracker.Resource", IsApplication=true)]
 
 namespace MoodTracker
 {
@@ -65,44 +63,32 @@ namespace MoodTracker
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050009
-			public const int button1 = 2131034121;
-			
 			// aapt resource value: 0x7f050005
-			public const int et_Email = 2131034117;
-			
-			// aapt resource value: 0x7f050008
-			public const int et_Password = 2131034120;
-			
-			// aapt resource value: 0x7f050003
-			public const int imageView1 = 2131034115;
-			
-			// aapt resource value: 0x7f05000a
-			public const int linearLayout1 = 2131034122;
-			
-			// aapt resource value: 0x7f050000
-			public const int space1 = 2131034112;
-			
-			// aapt resource value: 0x7f050001
-			public const int space2 = 2131034113;
-			
-			// aapt resource value: 0x7f050007
-			public const int space3 = 2131034119;
+			public const int button1 = 2131034117;
 			
 			// aapt resource value: 0x7f050002
-			public const int space4 = 2131034114;
+			public const int et_Email = 2131034114;
 			
 			// aapt resource value: 0x7f050004
-			public const int tv_Email = 2131034116;
+			public const int et_Password = 2131034116;
 			
-			// aapt resource value: 0x7f05000c
-			public const int tv_ForgotPW = 2131034124;
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
 			
 			// aapt resource value: 0x7f050006
-			public const int tv_Password = 2131034118;
+			public const int linearLayout1 = 2131034118;
 			
-			// aapt resource value: 0x7f05000b
-			public const int tv_Register = 2131034123;
+			// aapt resource value: 0x7f050001
+			public const int tv_Email = 2131034113;
+			
+			// aapt resource value: 0x7f050008
+			public const int tv_ForgotPW = 2131034120;
+			
+			// aapt resource value: 0x7f050003
+			public const int tv_Password = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int tv_Register = 2131034119;
 			
 			static Id()
 			{
@@ -120,6 +106,9 @@ namespace MoodTracker
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int Registration = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -133,11 +122,8 @@ namespace MoodTracker
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int ApplicationName = 2130968576;
 			
 			static String()
 			{
